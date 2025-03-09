127bd234b737527f49489b80186c67afd1e98123
GrachevAleksandr <grachaleksw@gmail.com>
2025-03-10 00:17:04 +0300

    korekting

--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,25 @@
-def filter_by_state(data_lists : list, state="EXECUTED") -> list:
+def filter_by_state(list_state: list, state="EXECUTED") -> list:
     """
     принимает список словарей и опционально значение для ключа state(по умолчанию
     'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению.
     """
     new_list = []
-    for element in data_lists:
+    for element in list_state:
         for key, value in element.items():
-            if value == state :
+            if value == state:
                 new_list.append(element)
 
     return new_list
 
 
-def sort_by_date(data_lists : list, type_of_sorting="decreasing") -> list:
+def sort_by_date(list_state: list, type_of_sorting="decreasing") -> list:
     """
     принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный
     по дате (date).
     """
     if type_of_sorting == "decreasing":
-        return  sorted(data_lists, key=lambda x: x["date"], reverse=True)
+        return sorted(list_state, key=lambda x: x["date"], reverse=True)
     else:
-        return sorted(data_lists, key=lambda x: x["date"])
-
+        return sorted(list_state, key=lambda x: x["date"])
