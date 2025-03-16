e93734c5e4ae3965ae40d2e65536e8e1c91b2d69
GrachevAleksandr <grachaleksw@gmail.com>
2025-03-16 16:35:11 +0300

    add tests

--- src/mask.py
+++ src/mask.py
@@ -5,13 +5,21 @@ def get_mask_account(account: Union[str, int]) -> str:
     """
     Функция принимает на вход номер счета и возвращает его маску.
     """
-    mask_account = str(account)
-    return f"**{mask_account[-4:]}"
+    str_account = str(account)
+    if str_account.isdigit() is not True:  # Проверка не цифры
+        raise ValueError("Неверный номере")
+    if len(str_account) != 20:  # Проверка на количество цифр
+        raise ValueError("Неверный номер")
+    return f"**{str_account[-4:]}"
 
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """
     Функция принимает на вход номер карты и возвращает ее маску.
     """
-    n = str(card_number)
-    return f"{n[0:4]} {n[4:6]}** **** {n[-4:]}"
+    str_card_number = str(card_number)
+    if str_card_number.isdigit() is not True:  # Проверка не цифры
+        raise ValueError("Неверный номер")
+    if len(str_card_number) != 16:  # Проверка на количество цифр
+        raise ValueError("Неверный номер")
+    return f"{str_card_number[0:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,14 @@
-def filter_by_state(list_state: list, state: str="EXECUTED") -> list:
+from mypy.types import AnyType
+
+
+def filter_by_state(list_state: list, state: str = "EXECUTED") -> list:
     """
     принимает список словарей и опционально значение для ключа state(по умолчанию
     'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению.
     """
+    if len(list_state) == 0:  # Проверка на пустую строку
+        raise ValueError("Нет данных")
     new_list = []
     for element in list_state:
         for key, value in element.items():
@@ -12,13 +17,16 @@ def filter_by_state(list_state: list, state: str="EXECUTED") -> list:
     return new_list
 
 
-def sort_by_date(list_state: list, reverse: bool=True) -> list:
+def sort_by_date(list_state: list, reverse: bool = True) -> list:
     """
     принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный
     по дате (date).
     """
+    if len(list_state) == 0:  # Проверка на пустую строку
+        raise ValueError("Нет данных")
     if reverse is True:
         return sorted(list_state, key=lambda x: x["date"], reverse=True)
     else:
         return sorted(list_state, key=lambda x: x["date"])
+
